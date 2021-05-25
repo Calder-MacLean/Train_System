87f6e290e4017a658c8f123b51488680171807bb
Calder-MacLean <caldmac08@icloud.com>
2021-05-25 16:51:52 +0100

    Add files via upload

---
+++ Business Objects/expressTrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Calder MacLean MAtric No:  40313018
+ * This class is the abstract class for train which has all the getters and setters that are used buy the sub classes of Train
+ * Modification Date 29/11/18
+*/
+
+namespace Business_Objects
+{
+    [Serializable]
+    public class expressTrain : Train
+    {
+        public override string stops
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
